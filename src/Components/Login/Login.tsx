b5c52595b2113e4a8b1a427819fd92f5c519d8d4
Santiago Sanchez <santiagosanchezcst@gmail.com>
2025-03-05 15:32:16 -0600

    Added the sign in object and it css, is not functional, just visual

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,4 +1,7 @@
 import './Login.css'
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+
 
 const Login = () => {
   return (
@@ -8,10 +11,21 @@ const Login = () => {
             <div className='input-box'>
                 <input type='text'
                 placeholder='Username' required />
+                <FaUser className='icon'/>
             </div>
-
-
-
+            <div className='input-box'>
+                <input type='password'
+                placeholder='Password' required />
+                <RiLockPasswordFill className='icon' />
+            </div>
+            <div className="remember-forgot">
+              <label><input type="checkbox" />Remember this device</label>
+              <a href='#'> Forgot password?</a>
+            </div>
+            <button type="submit">Login</button>
+            <div className="register-link">
+              <p>New user? <a href='#'>Register</a></p>
+            </div>
         </form>
     </div>
   )
